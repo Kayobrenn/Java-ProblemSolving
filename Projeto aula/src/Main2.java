3546cfeb9e45ae2f1ed1c48847da7d583ede8397
Kayo Brenno <kbreno822@gmail.com>
2025-03-10 21:21:09 -0300

    New files

---
+++ Projeto aula/src/Main2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Objeto objeto = new Objeto();
+
+        System.out.println("=====> Iniciando Sistema Empresarial <=====");
+        System.out.println("===== Menu Sistema =====");
+        System.out.println("1 - Sistema PDV");
+        System.out.println("2 - Cadastro de Mercadoria");
+
+
+
+
+        sc.close();
+    }
+}
---
+++ Projeto aula/src/Objeto.java
@@ -0,0 +1,8 @@
+class Objeto {
+    private String nameprod;
+    private String coditem;
+    private String tam_prod;
+    private String cor_prod;
+    private double valor_item;
+    private int quant_item;
+}
