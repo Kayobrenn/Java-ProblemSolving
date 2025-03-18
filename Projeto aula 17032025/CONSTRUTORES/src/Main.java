eeef6b4e2212aaab6380af0f58821e43e2a1d207
Kayo Brenno <kbreno822@gmail.com>
2025-03-17 21:17:10 -0300

    Aula 17/03

---
+++ Projeto aula 17032025/CONSTRUTORES/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+
+        Aluno a = new Aluno ("João", 1234);
+
+        System.out.println("Nome: " + a.getNome());
+        System.out.println("Matricula: " + a.getMatricula());
+
+    }
+
+}
