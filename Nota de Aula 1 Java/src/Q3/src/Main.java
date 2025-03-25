e625eb20ffe87b18fc081f8c0be7a9d39814fa49
Kayo Brenno <Kbreno822@gmail.com>
2025-03-25 00:43:29 -0300

    Java questions

--- Nota de Aula 1 Java/src/Q3/src/Main.java
+++ Nota de Aula 1 Java/src/Q3/src/Main.java
@@ -5,22 +5,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Produto produto = new Produto();
 
-        System.out.print("Digite o nome do produto: ");
-        produto.nome = sc.nextLine();
+        do {
+            System.out.println("\n====== MENU SISTEMA =======");
+            System.out.println("1 - Adicionar produto ao Estoque");
+            System.out.println("2 - Vender Produto");
+            System.out.println("3 - Exibir Estoque do Produto");
+            System.out.println("4 - Sair do Programa");
+            System.out.print("Digite a opção desejada:");
+            produto.opcao = sc.nextInt();
 
-        System.out.print("Digite a quantidade do produto: ");
-        produto.quantidade = sc.nextDouble();
+            sc.nextLine();
 
-        System.out.print("Digite o valor do produto: ");
-        produto.preco = sc.nextDouble();
+            switch (produto.opcao) {
+                case 1:
+                    System.out.println("=======Cadastramento de Produto=======");
 
-        System.out.println("======\ MENU SISTEMA /=======");
-        System.out.println("1 - Adicionar produto ao Estoque");
-        System.out.println("2 - Vender Produto");
-        System.out.println("3 - Exibir Estoque do Produto");
-        produto.opcao = sc.nextInt();
+                    System.out.print("\nDigite o nome do produto: ");
+                    produto.setNome(sc.nextLine());
 
-        while (produto.opcao != 3) {}
+                    System.out.print("Digite a quantidade do produto: ");
+                    produto.setQuantidade(sc.nextInt());
 
+                    System.out.print("Digite o valor da produto: ");
+                    produto.setPreco(sc.nextDouble());
+
+                    System.out.println("\nProduto cadastrado com sucesso!");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    if (Produto.getNome() == null || Produto.getNome().isEmpty()) {
+                        System.out.println("Nenhum produto cadastrado.");
+                    } else {
+                        System.out.println("===== PRODUTO CADASTRADO =====");
+                        System.out.println("Produto cadastrado: " + Produto.getNome());
+                        System.out.println("Quantidade em Estoque:" + produto.getQuantidade());
+                        System.out.println("Valor Cadastrado:" + produto.getPreco());
+                    }
+                    break;
+                case 4:
+                    System.out.println("Obrigado por utilizar o programa!");
+                    break;
+            }
+        } while (produto.opcao != 4);
     }
 }
--- Nota de Aula 1 Java/src/Q3/src/Produto.java
+++ Nota de Aula 1 Java/src/Q3/src/Produto.java
@@ -4,4 +4,27 @@ public class Produto {
     double quantidade;
     int opcao;
 
+    public static String getNome() {
+        return nome;
+    }
+
+    public static void setNome(String nome) {
+        Produto.nome = nome;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
 }
