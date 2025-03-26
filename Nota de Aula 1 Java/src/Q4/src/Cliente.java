305b7a309fdaab0fac93c3c85c96e163337bbaba
Kayo Brenno <Kbreno822@gmail.com>
2025-03-25 23:47:57 -0300

    Java questions

---
+++ Nota de Aula 1 Java/src/Q4/src/Cliente.java
@@ -0,0 +1,29 @@
+public class Cliente {
+    private double saldo;
+    private double valorsaque;
+    private String destinatario;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getValorsaque() {
+        return valorsaque;
+    }
+
+    public void setValorsaque(double valorsaque) {
+        this.valorsaque = valorsaque;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+}
